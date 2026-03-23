3235ffc0353463668670e624227b19be03625c4b
poketony <57011422+poketony@users.noreply.github.com>
2026-03-23 13:08:13 +0900

    텍스트 번역 추가 및 환경 다듬기
    모노리스 동향이 보일 때까지 잠정 중단합니다

--- 0.X30/cf/kr/3.RepackAllStorySb.bat
+++ 0.X30/cf/kr/3.RepackAllStorySb.bat
@@ -5,7 +5,8 @@ echo .sb 파일 리팩을 시작합니다...
 :: 현재 폴더의 모든 .sb 파일에 대해 루프 실행
 for %%f in (*.sb) do (
     echo 처리 중: %%f
-    python 0.sbtool.py import "%%f" "%%f.txt"
+    :: %%~nf는 파일명에서 확장자(.sb)를 제거한 이름만 가져옵니다.
+    python 0.sbtool.py import "%%f" "%%~nf.txt"
 )
 
 echo.
