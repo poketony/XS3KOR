dc67f8241d8a1c278a533d1e22c88546b5e2c9a6
poketony <57011422+poketony@users.noreply.github.com>
2026-05-07 11:24:30 +0900

    타이틀 그래픽 오류를 수정

---
+++ noesis-xenosaga3xtx-plugin.py
@@ -0,0 +1,56 @@
+import noesis
+import rapi
+
+def registerNoesisTypes():
+	handle = noesis.register("Xenosaga XTX Texture", ".xtx")
+	noesis.setHandlerTypeCheck(handle, xtxCheckType)
+	noesis.setHandlerLoadRGBA(handle, xtxGetRGBA)
+	return 1
+	
+XTX_TAG = 0x00585458 # XTX.
+	
+def xtxCheckType(data):
+	if len(data) &lt; 0x50:#If the file is smaller than the header size, quit.
+		return 0
+	bs = NoeBitStream(data)
+
+	if bs.readInt() != XTX_TAG:#if the header tag is wrong, quit.
+		return 0
+		
+	return 1
+
+def xtxGetRGBA(data, texList):
+	bs = NoeBitStream(data)
+	
+	#tag
+	bs.readInt()
+	#start of pixels
+	imageOffset = int(bs.readByte())
+	
+	#unknown
+	bs.readBytes(11)
+	
+	#width
+	w = int(bs.readUShort())
+	#swizzel indicator...?
+	swiz = int(bs.readUShort())
+	#height
+	h = int(bs.readUShort())
+	
+	imgPix = data[imageOffset:]
+	
+	if (swiz != 0x04):#if this is not 0x04, the only value I've found that is raw RGBA32, quit.
+		return 0
+	
+	#This next loop takes care of the half size alpha channel. The files max alpha at 0x80 instead of 0xFF
+	#this is here to spread the values so they max at 0xFF for fully opaque
+	#if there is a better way to do this, let me know.
+	
+	for i in range(0,w*h):#For each pixel
+		if imgPix[((i+1)*4)-1] &gt; 0:#if the alpha is not 0 (this is needed due to overflow of 0 - 1 = FF)
+			imgPix[((i+1)*4)-1] = ((imgPix[((i+1)*4)-1] * 2)-1)&amp;0xFF #Every fourth byte, perform operations to roughly double the value, then cap it to 8 bits
+	
+	pix32 = rapi.imageDecodeRaw(imgPix, w, h, "r8g8b8a8")
+	
+	texList.append(NoeTexture("xtx", w, h, pix32, noesis.NOESISTEX_RGBA32))
+	return 1</code></pre></div>
